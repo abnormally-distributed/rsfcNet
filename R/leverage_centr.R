ab63265b5ffa5b77cebee0b2c2374282e216e778
Brandon Vaughan <ilikescience1@gmail.com>
2018-07-08 21:47:35 -0400

    Fixed a minor error that resulted in column and row names not being attached to the output.

--- R/leverage_centr.R
+++ R/leverage_centr.R
@@ -3,10 +3,12 @@
 #' This function calculates the leverage centrality for a single graph.
 #' @param graphs An undirected igraph object.
 #' @param weighted By default weighted=FALSE, but can also be set to weighted=TRUE.
+#' @param col.names The names of each column (node labels). Checks global environment for "colnames" but may be assigned directly.
+#' @param row.names The names of each row (subject). Checks global environment for "rownames" but may be assigned directly.
 #' @return A matrix of the eigenvector centralities of each node for each subject.
 #' @export
 #' @author Alex Upton, Brandon Vaughan
-#' @details  Leverage centrality is a new measure of node centrality put forth by Joyce et al (2010). Developed specifically for use in functional brain network analysis, this measure has a favorable property of attempting to chracterize information flow in an undirected functional brain network. Leverage centrality compares the degree of a node to the degree of all its neighbors. A high degree node is not highly central according to leverage if all of its neighbors are also high degree. Furthermore, leverage centrality does not assume information flows along the shortest path or in a serial fashion, but rather focuses on the disparity in node degrees in a small neighborhood to quantify consolidation and dissemination of information locally (Joyce et al, 2010). A node with a high degree/strength is not necessarily one with a high centrality value. Leverage centrality defines centrality as having high degree/strength relative to the degree/strength of a node's neighbors. If the neighbors are also of high degree/strength the node is not considered a central node (hub). Keeping with its biological inspiration, leverage centrality does not assume information in a network flows in a serial fashion or only along the shortest path, but on how information flows within a local neighborhood of nodes.
+#' @details  Leverage centrality is a new measure of node centrality put forth by Joyce et al (2010). Developed specifically for use in functional brain network analysis, this measure has a favorable property of attempting to chracterize information flow in an undirected functional brain network. Leverage centrality compares the degree of a node to the degree of all its neighbors. A node with a high degree/strength is not necessarily one with a high centrality value. Leverage centrality defines centrality as having high degree/strength relative to the degree/strength of a node's neighbors. If the neighbors are also of high degree/strength the node is not considered a central node. Keeping with its biological inspiration, leverage centrality does not assume information in a network flows in a serial fashion or only along the shortest path, but on how information flows within a local neighborhood of nodes (Joyce et al, 2010).
 #'
 #' The formula for leverage centrality is given below, where k_i is the degree/strength of a node and k_j is the degree/strength of its neighbors.
 #'
@@ -25,8 +27,10 @@
 #'
 #' http://igraph.wikidot.com/r-recipes#toc10
 
-leverage_centr = function(graphs, weighted=FALSE){
+leverage_centr = function(graphs, weighted=FALSE, col.names=.GlobalEnv$colnames, row.names=.GlobalEnv$rownames){
   lev = pbapply::pbsapply(graphs, function(g) leverage_centr_single(g, weighted=weighted))
   lev = t(lev)
+  rownames(lev) = row.names
+  colnames(lev) = col.names
   return(lev)
 }
