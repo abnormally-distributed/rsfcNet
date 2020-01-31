7cdbb7a4cb68ad4d5e9f19fd7ecc6e6fc42ae5ea
Vaughan <bkv@musc.edu>
2020-01-31 14:27:36 -0500

    Some minor updates

--- R/closeness_centr.R
+++ R/closeness_centr.R
@@ -1,6 +1,7 @@
 #' Calculate closeness centrality for a list of graphs.
 #'
-#' This function is a convenience wrapper for igraph's closeness function and takes as an input a list of igraph objects.
+#' @description This function is a convenience wrapper for igraph's closeness function and takes as an input a list of igraph objects.
+#'
 #' @param graphs a list of igraph objects.
 #' @param col.names The names of each column (node labels).
 #' @param row.names The names of each row (subject).
@@ -21,7 +22,7 @@
 #' @seealso
 #'
 #' \code{\link[rsfcNet]{closeness_centr_mult}}
-#' \code{\link[rsfcNet]{curent_centr}}
+#' \code{\link[rsfcNet]{current_centr}}
 #'
 #' @references
 #' Fornito, A., Zalesky, A., & Bullmore, E. (2016). Centrality and Hubs. Chapter 5. Fundamentals of Brain Network Analysis, 137-161. doi:10.1016/b978-0-12-407908-3.00005-4
@@ -42,7 +43,7 @@ closeness_centr_mult = function (graphs, col.names = NULL, row.names = NULL)
 
 #' Calculate closeness centrality for a single graph.
 #'
-#' This function is a convenience wrapper for igraph's closeness function for consistency with rsfcNet's naming conventions.
+#' @description This function is a convenience wrapper for igraph's closeness function for consistency with rsfcNet's naming conventions.
 #' @param graph A network as an igraph object or matrix.
 #' @param weights a vector of edge weights. Defaults to the absolute value of the input network.
 #' @return A vector of centrality scores for each node.
